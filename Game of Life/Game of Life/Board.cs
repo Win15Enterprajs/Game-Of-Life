e25fa970083108ab37c50bec1b15adb993cce42b
Roppin.L <robin.ljungqvist@gmail.com>
2016-02-24 19:47:12 +0100

    Fixed the logic in count neighbour method.

--- Game of Life/Game of Life/Board.cs
+++ Game of Life/Game of Life/Board.cs
@@ -12,7 +12,7 @@ namespace Game_of_Life
         {
             int[,] temp = arrayInput;
             Random rnd = new Random();
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 10; i++)
             {
                 temp[rnd.Next(0 , arrayInput.GetLength(0)),rnd.Next(0, arrayInput.GetLength(1))] = rnd.Next(0, 2);
             }
@@ -22,24 +22,25 @@ namespace Game_of_Life
         {
             for (int i = 0; i < array.GetLength(0); i++)
             {
+                Console.Write(i);
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     if (array[i, j] == 1)
                     {
                         if (j == array.GetLength(1) - 1)
                         {
-                            Console.Write("* \r\n");
+                            Console.Write("*\r\n");
                         }
-                        Console.Write("* ");
+                        Console.Write("*");
                     }
                     else if (j == array.GetLength(1) - 1)
                     {
-                        Console.Write("  \r\n");
+                        Console.Write(" \r\n");
                         break;
                     }
                     else if (array[i, j] == 0)
                     {
-                        Console.Write("  ");
+                        Console.Write(" ");
                     }
                 }
             }
@@ -48,38 +49,63 @@ namespace Game_of_Life
         public int CountNeighbour(uint x, uint y, int[,] arrayToCheck)
         {
             int amountOfNeighbours = 0;
-            if (x - 1 > 0 && y - 1 > 0 && x - 1 > 0 && arrayToCheck[x - 1, y - 1] == 1)
+            if ( x != 0)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x - 1, y] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
-            if (x - 1 > 0 && arrayToCheck[x - 1, y] == 1)
+            if ( y != 0)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x, y - 1] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
-            if (y + 1 < arrayToCheck.GetLength(1) && x - 1 > 0 && arrayToCheck[x - 1, y + 1] == 1)
+            if (x != arrayToCheck.GetLength(0) - 1)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x + 1, y] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
-            if (y - 1 > 0 && arrayToCheck[x , y - 1] == 1)
+            if (y != arrayToCheck.GetLength(1) - 1)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x, y + 1] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
-            if (y + 1 < arrayToCheck.GetLength(1) - 1 && arrayToCheck[x , y + 1] == 1)
+            if (x != arrayToCheck.GetLength(0) - 1 && y != arrayToCheck.GetLength(1) - 1)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x + 1, y + 1] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
-            if (x + 1 < arrayToCheck.GetLength(0) && arrayToCheck[x + 1, y - 1] == 1)
+            if (x != 0 && y != 0)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x - 1, y - 1] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
-            if (x + 1 < arrayToCheck.GetLength(0) && arrayToCheck[x + 1, y] == 1)
+            if(x != 0 && y != arrayToCheck.GetLength(1) - 1)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x - 1, y + 1] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
-            if (x - 1 > 0 && y + 1 < arrayToCheck.GetLength(1) && arrayToCheck[x - 1, y + 1] == 1)
+            if(y != 0 && x != arrayToCheck.GetLength(0) - 1)
             {
-                amountOfNeighbours++;
+                if (arrayToCheck[x + 1, y - 1] == 1)
+                {
+                    amountOfNeighbours++;
+                }
             }
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
             return amountOfNeighbours;
         }
--- Game of Life/Game of Life/Program.cs
+++ Game of Life/Game of Life/Program.cs
@@ -10,12 +10,13 @@ namespace Game_of_Life
     {
         static void Main(string[] args)
         {
-            int[,] testArray = new int[50, 50];
+            int[,] testArray = new int[5, 5];
             Board test = new Board();
             test.addRandomValues(testArray);
             test.printArr(testArray);
-            int number = test.CountNeighbour(2, 5, testArray);
+            int number = test.CountNeighbour(0 ,4 , testArray);
             Console.WriteLine(number);
+            Console.ReadKey();
             // random comment here smilyface
 
         }
