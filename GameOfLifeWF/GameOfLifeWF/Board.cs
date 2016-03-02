7a2f571c3d4ce702e40a4ef3f66f431683de1040
Drunkenwhisp <Kevinkarlberg@hotmail.se>
2016-03-02 13:14:07 +0100

    Added Classes

---
+++ GameOfLifeWF/GameOfLifeWF/Board.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_Life
+{
+    class Board
+    {
+        /// <summary>
+        /// Counts the neighbours which have the value of 1 surrounding the x and y coordinates.
+        /// </summary>
+        /// <param name="x">Coordinate of x-axis.</param>
+        /// <param name="y">Coordinate of y-axis.</param>
+        /// <param name="arrayToCheck">2D int Array of which to count from.</param>
+        /// <returns>Number of live neighbours the x and y coordinate has.</returns>
+        public int CountNeighbour(int x, int y, int[,] arrayToCheck)
+        {
+            int amountOfNeighbours = 0;
+            if (x != 0)                            /// Makes sure that you cant check below the limit of the X -axis.
+            {
+                if (arrayToCheck[x - 1, y] == (int)Cells.CellState.Alive || arrayToCheck[x - 1, y] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            if (y != 0)                            /// Makes sure that you can't check below the limit of the Y -axis.
+            {
+                if (arrayToCheck[x, y - 1] == (int)Cells.CellState.Alive || arrayToCheck[x, y - 1] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            if (x != arrayToCheck.GetLength(0) - 1) /// Makes sure that you can't check above the limit of the X -axis.
+            {
+                if (arrayToCheck[x + 1, y] == (int)Cells.CellState.Alive || arrayToCheck[x + 1, y] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            if (y != arrayToCheck.GetLength(1) - 1) /// Makes sure that you can't check above the limit of the Y -axis.
+            {
+                if (arrayToCheck[x, y + 1] == (int)Cells.CellState.Alive || arrayToCheck[x, y + 1] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            if (x != arrayToCheck.GetLength(0) - 1 && y != arrayToCheck.GetLength(1) - 1) /// Makes sure that you can't check above the limit of the X and Y -axis.
+            {                                                                            ///  Which means the bottom right corner of the matrix.
+                if (arrayToCheck[x + 1, y + 1] == (int)Cells.CellState.Alive || arrayToCheck[x + 1, y + 1] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            if (x != 0 && y != 0)                           /// Makes sure that you can't check below the limit of the Y and X -axis.
+            {                                               /// Which means the upper left corner of the matrix.    
+                if (arrayToCheck[x - 1, y - 1] == (int)Cells.CellState.Alive || arrayToCheck[x - 1, y - 1] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            if (x != 0 && y != arrayToCheck.GetLength(1) - 1) /// Makes sure that you can't check below the limit of the X -axis or above the limit of the Y -axis.
+            {                                                /// Which means the upper right corner of the matrix.
+                if (arrayToCheck[x - 1, y + 1] == (int)Cells.CellState.Alive || arrayToCheck[x - 1, y + 1] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            if (y != 0 && x != arrayToCheck.GetLength(0) - 1) /// Makes sure that you can't check below the limit of the X -axis or above the limit of the Y -axis.
+            {                                                /// Which means the bottom left corner of the matrix.
+                if (arrayToCheck[x + 1, y - 1] == (int)Cells.CellState.Alive || arrayToCheck[x + 1, y - 1] == (int)Cells.CellState.aboutToDie)
+                {
+                    amountOfNeighbours++;
+                }
+            }
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            return amountOfNeighbours;
+        }
+        public void printArr(int[,] array)
+        {
+            char asciiAlive = '█';
+            char asciiDead = ' ';
+            char asciiAboutToDie = '░';
+            char asciiAboutToBeReborn = '▒';
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == (int)Cells.CellState.Alive) // 3 Alive
+                    {
+
+                        if (j == array.GetLength(1) - 1)
+                        {
+                            sb.AppendFormat("{0}\n", asciiAlive);
+                        }
+                        else
+                        {
+                            sb.AppendFormat("{0}", asciiAlive);
+                        }
+                    }
+                    else if (array[i, j] == (int)Cells.CellState.aboutToBeReborn) // 2 About to be reborn.
+                    {
+                        if (j == array.GetLength(1) - 1)
+                        {
+                            sb.AppendFormat("{0}\n", asciiAboutToBeReborn);
+                        }
+                        else
+                        {
+                            sb.AppendFormat("{0}", asciiAboutToBeReborn);
+                        }
+                    }
+                    else if (array[i, j] == (int)Cells.CellState.aboutToDie) // 1 About to die.
+                    {
+                        if (j == array.GetLength(1) - 1)
+                        {
+                            sb.AppendFormat("{0}\n", asciiAboutToDie);
+                        }
+                        else
+                        {
+                            sb.AppendFormat("{0}", asciiAboutToDie);
+                        }
+                    }
+                    else if (array[i, j] == 0 && j == array.GetLength(1) - 1) // 0 Dead
+                    {
+                        sb.AppendFormat("{0}\n", asciiDead);
+                        break;
+                    }
+                    else if (array[i, j] == 0)
+                    {
+                        sb.AppendFormat("{0}", asciiDead);
+                    }
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+        public int[,] addRandomValues(int[,] arrayInput)
+        {
+            int amountToAdd = (arrayInput.GetLength(0) * arrayInput.GetLength(1)) / 4;
+            int[,] tempArray = arrayInput;
+            Random rnd = new Random();
+            for (int i = 0; i < amountToAdd; i++)
+            {
+                tempArray[rnd.Next(0 , arrayInput.GetLength(0)),rnd.Next(0, arrayInput.GetLength(1))] = rnd.Next(0, 2);
+            }
+            return tempArray;
+        }
+        public void PrintTheArray(int[,] array) //For visual testing purposes.
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.Write("|");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(" " + array[i, j] + " |");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("--------------------------------------------------------------");
+        }
+    }
+}
---
+++ GameOfLifeWF/GameOfLifeWF/Cells.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Game_of_Life
+{
+    class Cells
+    {
+        public enum CellState
+        {
+            Dead,
+            Alive,
+            aboutToDie,
+            aboutToBeReborn
+        }
+        public int[,] MakeTheTempArray(int[,] array)
+        {
+            var tempArray = new int[array.GetLength(0), array.GetLength(1)];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    tempArray[i, j] = array[i, j];
+                }
+            }
+            return tempArray;
+        }
+        /// <summary>
+        ///  Manipulates the cells in the matrix according to the rules of the Game of Life
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public int[,] ManipulateCells(int[,] array)
+        {
+            var board = new Board();
+            var tempArray = MakeTheTempArray(array);
+            int numberOfNeighbours = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    numberOfNeighbours = board.CountNeighbour(i, j, array);
+                    if (array[i,j] == (int)CellState.aboutToDie)
+                    {
+                        tempArray[i, j] = 0;
+                    }   
+                    else if (array[i,j] == (int)CellState.aboutToBeReborn)
+                    {
+                        tempArray[i, j] = 1;
+                    } 
+                    else if (array[i, j] == 1)
+                    {
+                        //  if (board.CountNeighbour(i,j,array) != 2 || board.CountNeighbour(i, j, array) != 3)
+                        if (numberOfNeighbours < 2 || numberOfNeighbours > 3)
+                        {
+                            tempArray[i, j] = 2; //=(int)CellState.aboutToDie;
+                        }
+
+                        
+                    }
+                    else if (numberOfNeighbours == 3)
+                    {
+                        tempArray[i, j] = 3; //=(int)CellState.aboutToBeReborn;
+                    }
+                }
+            }
+
+            return tempArray;
+        }
+        /// <summary>
+        /// Offers the user an option to automate the cellupdates according to a pre-set or use decided pace
+        /// </summary>
+        public int AutomatedCellUpdate()
+        {
+            var board = new Board();
+            Console.WriteLine("Type in the number of miliseconds you wish inbetween each cellupdate, or press R for [R]ecommended cellupdate timer");
+            string userChoice = Console.ReadLine();
+            int miliseconds = 0;
+            if (userChoice.ToLower() == "r")
+            {
+                return 400;
+            }
+            else
+            {
+                while (!int.TryParse(userChoice, out miliseconds))
+                {
+                    Console.WriteLine("You did not enter a correct format, please try again");
+                    userChoice = Console.ReadLine();
+                }
+                return miliseconds;
+            }
+            
+        }
+    }
+}
